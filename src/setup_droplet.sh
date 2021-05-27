4a6dc2cc7cb007398a9254cdc985b91f03c3a94c
Allen Ng <allenng@Allens-MacBook-Air.local>
2021-05-28 01:20:13 +0800

    modify files

--- src/setup_droplet.sh
+++ src/setup_droplet.sh
@@ -6,26 +6,4 @@ yum -y install epel-release
 yum -y install mosquitto
 yum -y install python3.8
 yum -y install tmux
-#configure mosquitto on startup
-echo "configure mosquitto on startup"
-systemctl start mosquitto
-systemctl enable mosquitto
-#setup password
-#password stored in passwd file
-echo "configuring username & password..."
-echo "please key in username: "
-read username
-mosquitto_passwd -c /etc/mosquitto/passwd $username
-echo "please key in password: "
-#configure mosquitto.conf to use passwd file
-echo "configure mosquitto.conf to use passwd file"
-rm -f /etc/mosquitto/mosquitto.conf
-echo "please press Ctrl + C"
-cat > /etc/mosquitto/mosquitto.conf 
-allow_anonymous false 
-password_file /etc/mosquitto/passwd
-echo "Restarting MQTT broker"
-systemctl restart mosquitto
-echo "installing python dependencies"
-pip3 install -r requirements.txt
-echo "setup complete, please configure src/paho-mqtt-client.py based on your mqtt topic & machine advisor credentials"
+yum -y install vim
